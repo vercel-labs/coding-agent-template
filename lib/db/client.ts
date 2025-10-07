a27268934e09fccc1cdeba3314a3391a0facbdfd
Chris Tate <chris@ctate.dev>
2025-10-07 12:59:45 -0500

    add github action for pr checks (#20)
    * add github action for pr checks
    
    * safe db init

--- lib/db/client.ts
+++ lib/db/client.ts
@@ -2,9 +2,17 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from './schema'
 
-if (!process.env.POSTGRES_URL) {
-  throw new Error('POSTGRES_URL environment variable is required')
-}
+let _db: ReturnType<typeof drizzle> | null = null
 
-const client = postgres(process.env.POSTGRES_URL)
-export const db = drizzle(client, { schema })
+export const db = new Proxy({} as ReturnType<typeof drizzle>, {
+  get(target, prop) {
+    if (!_db) {
+      if (!process.env.POSTGRES_URL) {
+        throw new Error('POSTGRES_URL environment variable is required')
+      }
+      const client = postgres(process.env.POSTGRES_URL)
+      _db = drizzle(client, { schema })
+    }
+    return Reflect.get(_db, prop)
+  },
+})
