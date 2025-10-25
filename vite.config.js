ef0fedde0b5eb8c9f422899791a9d141c1779a05
Chris Tate <ctate@users.noreply.github.com>
2025-10-25 17:51:05 +0000

    Handle dynamic sandbox subdomain to avoid error

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   server: {
     host: '0.0.0.0',
-    allowedHosts: [
-      '.vercel.run',
-      'sb-37qid9ih7grp.vercel.run'
-    ]
+    allowedHosts: true
+  },
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: true
   }
 })
